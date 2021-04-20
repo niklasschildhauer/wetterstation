ea62081f5b2822ea2d86c2989d698fe1c3533fd3
Alexander Merker <am206@hdm-stuttgart.de>
2021-04-20 16:47:38 +0200

    initialize project

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+console.log("listening on port 4201")
+app.listen(4201);
