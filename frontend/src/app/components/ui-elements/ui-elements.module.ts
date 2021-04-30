8ddb5d887d7dba12e16faa7da0e9a5a787e51462
Niklas Schildhauer <ns162@hdm-stuttgart.de>
2021-04-30 19:07:22 +0200

    FE-7-implemented Navbar

--- frontend/src/app/components/ui-elements/ui-elements.module.ts
+++ frontend/src/app/components/ui-elements/ui-elements.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackButtonElementComponent } from './back-button-element/back-button-element.component';
+import { NavBarElementComponent } from './nav-bar-element/nav-bar-element.component';
 
 
 
 @NgModule({
   declarations: [
-    BackButtonElementComponent
+    BackButtonElementComponent,
+    NavBarElementComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    BackButtonElementComponent
+    BackButtonElementComponent,
+    NavBarElementComponent
   ]
 })
 export class UiElementsModule { }
