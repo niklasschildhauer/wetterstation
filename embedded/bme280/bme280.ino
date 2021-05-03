f945d87763fab20afbdef08e54c3b871fffbe1b5
Kim <kb112@hdm-stuttgart.de>
2021-05-03 21:42:31 +0200

    added code for the bme version to connect to the wifi - not tested with endpoint yet

--- embedded/bme280/bme280.ino
+++ embedded/bme280/bme280.ino
@@ -1,9 +1,15 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 
 Adafruit_BME280 bme; // I2C
+
+const char* ssid     = "Vodafone-1F7F"; //name of your wifi
+const char* password = "ABbpgU3aGaNmedUm"; //pw of your wifi
+const char* server_ip = "127.0.0.1:2019"; //add server ip and port
 
 void setup() {
   
@@ -29,6 +35,27 @@ void setup() {
   else {
     Serial.println("Communication established!\n");
   }
+
+   // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    WiFiClient client;
+    HTTPClient http;
  }
 
 void loop() {
@@ -43,5 +70,43 @@ void loop() {
  Serial.print(bme.readHumidity());
  Serial.println(" %\n");
  delay(1000);
- 
+
+ WiFiClient client;
+  HTTPClient http;
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+   
+
+    Serial.print("[HTTP] begin...\n");
+    // configure target server and url
+//    http.begin(client, "http://"server_ip"/mqdata"); //HTTP //error in this line > might be fixed by a running server
+    http.addHeader("Content-Type", "application/json");
+
+
+    Serial.print("[HTTP] POST...\n");
+    // start connection and send HTTP header and body
+    char buffer[200];
+
+    sprintf(buffer, "{\"air quality\":\"%d\"}", bme.readTemperature());
+    Serial.println(buffer);
+    int httpCode = http.POST(buffer);
+   
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");    
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
 }
