9c258ed7d78c64866a12d5648f7b669c25d5007c
Kim <kb112@hdm-stuttgart.de>
2021-05-04 16:03:51 +0200

    added code to the dataService index.js

--- services/data-service/index.js
+++ services/data-service/index.js
@@ -9,5 +9,18 @@ app.get('/', function(req, res){
     res.status(200).send("Hello from port " + port);
 });
 
+app.post('/sensorout', (req, res) => {
+	var temperature = req.body.temperature;
+	var pressure = req.body.pressure;
+	var humidity = req.body.humidity;
+});
+
+app.post('/sensorin', (req, res) => {
+	var roomTemperature = req.body.temperature;
+	var roomPressure = req.body.pressure;
+	var roomHumidity = req.body.humidity;
+	var gasVal = req.body.gasVal;
+})
+
 console.log("listening on port", port)
 app.listen(port);
