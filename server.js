d7dbe3d12bdc235440d4ba626782d4fdef987425
Alexander Merker <alex.rekrem@gmx.de>
2021-05-04 17:41:45 +0200

    implement api gw access for data-service

--- server.js
+++ server.js
@@ -17,45 +17,45 @@ const request = require("request");
 
 //Allow CORS, because Angular is running on a different port (4200)
 const corsOptions = {
-   origin: "http://localhost:4200",
-   allowedHeaders: [
-     "Origin",
-     "X-Requested-With",
-     "Content-Type",
-     "Accept",
-     "Authorization",
-     "x-xsrf-token"
-   ]
- };
- 
- let swaggerOptions = {
-   swaggerDefinition: {
-     info: {
-       description: "Wetterstation API GW",
-       title: "Wetterstation API",
-       version: "1.0.0"
-     },
-     host: "localhost:4201",
-     basePath: "/v1",
-     produces: ["application/json", "application/xml"],
-     schemes: ["http"],
-     securityDefinitions: {
-       JWT: {
-         type: "apiKey",
-         in: "header",
-         name: "Authorization",
-         description: ""
-       }
-     }
-   },
-   basedir: __dirname, //app absolute path
-   files: ["./server.js"] //Path to the API handle folder
- };
- expressSwagger(swaggerOptions);
- 
- app.use(cors(corsOptions));
- app.use(morgan("dev"));
- app.use(express.json({ limit: "1mb", type: "application/json" }));
+  origin: "http://localhost:4200",
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "x-xsrf-token"
+  ]
+};
+
+let swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      description: "Wetterstation API GW",
+      title: "Wetterstation API",
+      version: "1.0.0"
+    },
+    host: "localhost:4201",
+    basePath: "/v1",
+    produces: ["application/json", "application/xml"],
+    schemes: ["http"],
+    securityDefinitions: {
+      JWT: {
+        type: "apiKey",
+        in: "header",
+        name: "Authorization",
+        description: ""
+      }
+    }
+  },
+  basedir: __dirname, //app absolute path
+  files: ["./server.js"] //Path to the API handle folder
+};
+expressSwagger(swaggerOptions);
+
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json({ limit: "1mb", type: "application/json" }));
 
 
 
@@ -73,7 +73,20 @@ const corsOptions = {
  * @property {string} success
  */
 
+/**
+* @typedef SensordataOutdoors
+* @property {integer} temperature.required
+* @property {integer} humidity.required
+* @property {integer} pressure.required
+*/
 
+/**
+* @typedef SensordataIndoors
+* @property {integer} roomTemperature.required
+* @property {integer} roomHumidity.required
+* @property {integer} roomPressure.required
+* @property {integer} gasVal.required
+*/
 
 
 // ------------------------------------------------ Routes ------------------------------------------------
@@ -87,15 +100,15 @@ const corsOptions = {
  * @returns {Error}  Http 400 - Bad Request if user credentials are not correct
  */
 app.post("/v1/auth/login", (req, res) => {
-   genericRequestWithPayload(
-     "",
-     "POST",
-     "http://localhost:4202/login",
-     JSON.stringify(req.body),
-     res
-   );
- });
- 
+  genericRequestWithPayload(
+    "",
+    "POST",
+    "http://localhost:4202/login",
+    JSON.stringify(req.body),
+    res
+  );
+});
+
 /**
  * Check a token for validity
  * @route GET /auth/checkToken
@@ -103,67 +116,90 @@ app.post("/v1/auth/login", (req, res) => {
  * @security JWT
  * @returns {Auth_Response.model} Authentication response message
  */
- app.get("/v1/auth/checkToken", (req, res) => {
-   const token = req.headers["x-access-token"] || req.headers["authorization"];
-   genericRequest(token, "GET", "http://localhost:4202/checkToken", res);
- });
+app.get("/v1/auth/checkToken", (req, res) => {
+  const token = req.headers["x-access-token"] || req.headers["authorization"];
+  genericRequest(token, "GET", "http://localhost:4202/checkToken", res);
+});
 
+//TODO: Returns?
+/**
+ * Receive data for outdoor sensors
+ * @route POST /sensors/outdoor
+ * @group sensors - Receiving of sensor data
+ * @param {SensordataOutdoors.model} sensordata.body.required test
+ */
+app.post("/v1/sensors/outdoor", (req, res) => {
+  genericRequestWithPayload("", "POST", "http://localhost:4204/sensorout", JSON.stringify(req.body), res);
+});
+
+
+//TODO: Returns?
+/**
+ * Receive data for outdoor sensors
+ * @route POST /sensors/indoor
+ * @group sensors - Receiving of sensor data
+ * @param {SensordataIndoors.model} sensordata.body.required test
+ */
+app.post("/v1/sensors/indoor", (req, res) => {
+  genericRequestWithPayload("", "POST", "http://localhost:4204/sensorin", JSON.stringify(req.body), res);
+});
 
 
 
 // ------------------------------------------------ Helper ------------------------------------------------
 
+
 const genericRequest = (token, method, uri, res) => {
-   request(
-     {
-       headers: {
-         Authorization: token,
-         "Content-Type": "application/json",
-         Accept: "application/json"
-       },
-       uri: uri,
-       method: method
-     },
-     function(error, response, body) {
-       genericCallback(error, response, body, res);
-     }
-   );
- };
- 
- const genericRequestWithPayload = (token, method, uri, body, res) => {
-   request(
-     {
-       headers: {
-         Authorization: token,
-         "Content-Type": "application/json",
-         Accept: "application/json"
-       },
-       uri: uri,
-       method: method,
-       body: body
-     },
-     function(error, response, body) {
-       genericCallback(error, response, body, res);
-     }
-   );
- };
- 
- const genericCallback = (error, response, body, res) => {
-   if (error) {
-     res.sendStatus("400");
-   } else {
-     if (response.statusCode === 200) {
-       let data = JSON.parse(body);
-       res.json(data);
-     } else if (response.statusCode === 401) {
-       let out = "The request is unauthorized";
-       res.status("401").json(out);
-     } else {
-       let out = "Bad request";
-       res.status("400").json(out);
-     }
-   }
- };
+  request(
+    {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      uri: uri,
+      method: method
+    },
+    function (error, response, body) {
+      genericCallback(error, response, body, res);
+    }
+  );
+};
+
+const genericRequestWithPayload = (token, method, uri, body, res) => {
+  request(
+    {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      uri: uri,
+      method: method,
+      body: body
+    },
+    function (error, response, body) {
+      genericCallback(error, response, body, res);
+    }
+  );
+};
+
+const genericCallback = (error, response, body, res) => {
+  if (error) {
+    res.sendStatus("400");
+  } else {
+    if (response.statusCode === 200) {
+      let data = JSON.parse(body);
+      res.json(data);
+    } else if (response.statusCode === 401) {
+      let out = "The request is unauthorized";
+      res.status("401").json(out);
+    } else {
+      let out = "Bad request";
+      res.status("400").json(out);
+    }
+  }
+};
 
 
 
@@ -182,7 +218,7 @@ CURL test comannds WINDOWS
 
 curl -X GET http://localhost:4202/checkToken
 // --> invalid token
-curl -H "Content-Type: application/json" -X POST http://localhost:4202/login -d "{\"password\":\"password\", \"username\":\"admin\"}" 
+curl -H "Content-Type: application/json" -X POST http://localhost:4202/login -d "{\"password\":\"password\", \"username\":\"admin\"}"
 // --> 200, receive token
 curl -X GET -H "Authorization: Bearer <received_token>" http://localhost:4202/checkToken
 //Logout
--- services/data-service/index.js
+++ services/data-service/index.js
@@ -4,15 +4,15 @@ const port = 4204;
 
 var express = require('express');
 var app = express();
- 
+
 //app.use(express.urlencoded({
 //	extended:true
 //}));
 app.use(express.json());
 
 
-app.get('/', function(req, res){
-    res.status(200).send("Hello from port " + port);
+app.get('/', function (req, res) {
+	res.status(200).send("Hello from port " + port);
 });
 
 app.post('/sensorout', (req, res) => {
@@ -20,7 +20,7 @@ app.post('/sensorout', (req, res) => {
 	var pressure = req.body.pressure;
 	var humidity = req.body.humidity;
 	console.log(req.body);
-	res.status(200).send("OK");
+	res.status(200).json("OK");
 });
 
 app.post('/sensorin', (req, res) => {
@@ -29,7 +29,7 @@ app.post('/sensorin', (req, res) => {
 	var roomHumidity = req.body.humidity;
 	var gasVal = req.body.gasVal;
 	console.log(req.body);
-	res.status(200).send("OK");
+	res.status(200).json("OK");
 });
 
 console.log("listening on port", port)
