270a14c6051fc2cd7da1d188d1d0bf876d987f77
Kim <kb112@hdm-stuttgart.de>
2021-05-16 21:03:43 +0200

    updated index.ts

--- services/Persistentdata-smarthome/src/index.ts
+++ services/Persistentdata-smarthome/src/index.ts
@@ -6,25 +6,25 @@ import express from 'express';
 
 "use strict"
 
-const port = 4205;
 
-var express = require('express');
-var app = express();
-
-//app.use(express.urlencoded({
-//  extended:true
-//}));
-app.use(express.json());
+// create typeorm connection
+createConnection().then(connection => {
+    const outdoorData = connection.getRepository(Outdoor);
 
 
-app.get('/', function (req, res) {
-    res.status(200).send("Hello from port " + port);
-});
+    //set up express 
+    const port = 4205;
+    var express = require('express');
+    var app = express();
+    app.use(express.json());
 
 
+   /* app.get('/', function (req, res) {
+        res.status(200).send("Hello from port " + port);
+    });
 
 
-
+/*
 // Connection to the database entitiy
 createConnection().then(async connection => {
 
@@ -41,26 +41,26 @@ createConnection().then(async connection => {
     console.log("Loaded outdoors: ", outdoors);
 
 }).catch(error => console.log(error));
+*/
 
 
 
-function sendToDatabase(query, queryparams){
+    app.get('/outdoorall', async (req, res) => {
+            const outdoors = await outdoorData.find();
+            res.json(outdoors);
+            console.log(outdoors);
+        });
 
-   createConnection().then(async connection => {
-
-        console.log("Inserting new sensor data...");
-        const outdoor = new Outdoor();
-        outdoor.humidity = 30;
-        outdoor.temperature = 24;
-        outdoor.pressure = 7;
-        await connection.manager.save(outdoor);
-        console.log("Saved new outdoor data with id: " + outdoor.id);
-    }).catch(error => console.log(error));
-}
-
+    app.post('/outdoorinsert', async (req, res) => {
+        const outdoor = await outdoorData.create(req.body);
+        const results = await outdoorData.save(outdoor);
+        return res.send(results);
+    });
 
 
 
 
-console.log("listening on port", port)
-app.listen(port);
+    console.log("listening on port", port)
+    app.listen(port);
+
+});
