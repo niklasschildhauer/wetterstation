f88dcaf876e409acd57a2205a567305986cf69d5
Niklas Schildhauer <ns162@hdm-stuttgart.de>
2021-05-17 11:04:12 +0200

    FE-13-personalization view

--- frontend/src/app/components/ui-elements/checkbox-switcher-element/checkbox-switcher-element.component.ts
+++ frontend/src/app/components/ui-elements/checkbox-switcher-element/checkbox-switcher-element.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Themes, UserContext } from 'src/app/model/user-context';
+import { UserContextService } from 'src/app/services/user-context.service';
 
 @Component({
   selector: 'app-checkbox-switcher-element',
@@ -8,17 +11,56 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class CheckboxSwitcherElementComponent implements OnInit {
   @Input() label?: string;
   @Input() value?: boolean;
-  @Input() checkedImage?: string;
-  @Input() unCheckedImage?: string;
+  @Input() checkedImage?: string = "microphone"
+  @Input() unCheckedImageLight?: string = "reduce-motion-unchecked-light";
+  @Input() unCheckedImageDark?: string = "reduce-motion-unchecked-dark";
   @Output() toggled = new EventEmitter<boolean>();
+  private userContext?: UserContext
+  private systemTheme = Themes.Light
 
-  constructor() { }
+  constructor(private userContextService: UserContextService,               
+    private breakpointObserver: BreakpointObserver,
+    ) { }
 
   ngOnInit(): void {
+    this.loadUserContext();
+    this.systemThemeBreakpointObserver();
   }
 
   toggleCheckbox() {
     this.toggled.emit(!this.value);
   }
+  private loadUserContext() {
+    this.userContextService.getUserContext().subscribe(data => this.userContext = data);
+  }
+
+  private systemThemeBreakpointObserver() {
+    this.breakpointObserver
+    .observe(['(prefers-color-scheme: light)'])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.systemTheme = Themes.Light;
+      } else {
+        this.systemTheme = Themes.Dark;
+      }
+    });
+  }
+
+  getUncheckedImageSrc(): string | undefined {
+    let userContextTheme = this.userContext?.theme
+    if(userContextTheme == Themes.Automatic) {
+      if(this.systemTheme == Themes.Dark) {
+        return this.unCheckedImageDark
+      } else {
+        return this.unCheckedImageLight
+      }
+    } 
+    if(userContextTheme == Themes.Dark) {
+      return this.unCheckedImageDark
+    }
+    return this.unCheckedImageLight
+
+  }
+
 
 }
