30b3c4bc00a0c3fb44898e6fc30230d456333448
Alexander Merker <alex.rekrem@gmx.de>
2021-05-31 19:20:09 +0200

    impl bme280 outdoors sensor connection

--- embedded/bme280/bme280.ino
+++ embedded/bme280/bme280.ino
@@ -3,19 +3,72 @@
 #include <Adafruit_BME280.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
+#include <WebServer.h>
+#include <AutoConnect.h>
 
+StaticJsonDocument<300> doc;
+
+WebServer Server;
+AutoConnect Portal(Server);
+AutoConnectConfig Config;
+AutoConnectAux auxUpload;
+
+void rootPage()
+{
+  char content[] = "it works";
+  Server.send(200, "text/plain", content);
+}
 
 Adafruit_BME280 bme; // I2C
 
-const char* ssid     = "FRITZ!Box 7590 UK";                   //"Vodafone-1F7F"; //name of your wifi
-const char* password = "81352968356302772244";                 //"ABbpgU3aGaNmedUm"; //pw of your wifi
-const char* serverip = "192.168.0.136:4204"; //add server ip and port
+const char* ssid     = "";                   //""; //name of your wifi
+const char* password = "";                 //""; //pw of your wifi
 #define SERVER_IP "192.168.178.30:4201"                                //"192.168.0.136:4201"
+
+char *roomName;
+char *transmissionFrequency;
+char *postalCode;
+
+char *sanitizeStr(String s)
+{
+  char delimiter[] = "\"";
+  char Buf[50];
+  char *token;
+  s.toCharArray(Buf, 50);
+  token = strtok(Buf, delimiter);
+  token = strtok(NULL, delimiter);
+  //Serial.println("token");
+  //Serial.println(token);
+  return token;
+}
 
 void setup() {
   
   Serial.begin(9600);
 
+  /*
+  * -----------------------------------------------------------------------------
+  *       Keep this code for debugging purposes
+  *       to simulate "1st install" and wipe all stored credentials
+  *       in the ESP32 flash memory
+  */
+
+  //Erase flash memory ssid to force to use our config file
+  //WiFi.mode(WIFI_STA);
+  //delay(100);
+  //WiFi.begin();
+  //if (WiFi.waitForConnectResult() == WL_CONNECTED)
+  //{
+  //  WiFi.disconnect();
+  //  while (WiFi.status() == WL_CONNECTED)
+  //    delay(100);
+  //}
+  //Serial.println("Flash memory should be erased");
+
+  // -----------------------------------------------------------------------------
+  
  
  // 0x76 and 0x77 are possible
  // bool communication = bme.begin();
@@ -30,36 +83,57 @@ void setup() {
    Serial.println(bme.sensorID(), 16);
    Serial.println("ID of 0xFF probably means a bad address\n");
    
-  while (true) { };
-    delay(10);
-  }
+    while (true) { };
+      delay(10);
+    }
   else {
     Serial.println("Communication established!\n");
   }
 
-   // We start by connecting to a WiFi network
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    //Test filesystem access
+    if (!SPIFFS.begin(true))
+    {
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
     }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
 
-    WiFiClient client;
-    HTTPClient http;
+    File file = SPIFFS.open("/ESPconfig.txt");
+    if (!file)
+    {
+      Serial.println("Failed to open file for reading");
+      return;
+    }
+
+    JsonArray arr = doc.to<JsonArray>();
+    for (int i = 0; i <= 7; i++)
+    {
+      String s = file.readStringUntil('\n');
+      arr.add(s);
+    }
+
+    //Fill variables from config file
+    char a[50];
+    char b[50];
+    char c[50];
+    strcpy(a, sanitizeStr(arr[1]));
+    strcpy(b, sanitizeStr(arr[2]));
+    strcpy(c, sanitizeStr(arr[3]));
+    roomName = a;
+    transmissionFrequency = b;
+    postalCode = c;
+
+    Server.on("/", rootPage);
+
+    if (Portal.begin())
+    {
+      Serial.println("Wifi connected to esp gedingsel: " + WiFi.localIP().toString());
+    }
  }
 
 void loop() {
+
+ //WIFI Autoconfig-Page
+ Portal.handleClient();
   
  Serial.print("Temperature = ");
  Serial.print(bme.readTemperature());
@@ -111,5 +185,6 @@ void loop() {
     }
 
     http.end();
+    delay(50000);
   }
 }
